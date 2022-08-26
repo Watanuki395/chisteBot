897da339421f8c73bca91292c7ff59357ff7f151
Gerardo Adolfo Salas Montoya <gesalas@kyndryl.com>
2022-08-25 22:38:05 -0600

    adding new message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Los mejores chistes
+          Dale clic a la burbuja del chatbot en la parte inferior derecha para comenzar.
         </p>
       </header>
     </div>
